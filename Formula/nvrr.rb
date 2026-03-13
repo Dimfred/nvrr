8c55ed990dd389cc17df68b7b71439bd612bd098
Dmitrij Vinokour <vinokour.dmitrij@gmail.com>
2026-03-13 14:46:30 +0100

    chore: formula update

--- Formula/nvrr.rb
+++ Formula/nvrr.rb
@@ -1,8 +1,8 @@
 class Nvrr < Formula
   desc "Simple neovim-remote in Rust"
   homepage "https://github.com/Dimfred/nvrr"
-  url "https://github.com/Dimfred/nvrr/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 ""
+  url "https://github.com/Dimfred/nvrr/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "b2143d0e7aba302ebac5351aaa0852954168de5f8fc29f17329f02ba6495eb94"
   license "MIT"
 
   depends_on "rust" => :build
